3047803bf28e11c86c69aa45c48fab267f482e1e
William Pacetti <wspacetti@gmail.com>
2017-05-03 16:56:42 -0500

    putting variables in template to render ish

--- app/components/application.vue
+++ app/components/application.vue
@@ -4,15 +4,12 @@
       <div class="panel">
         <div class="panel-block">
           <input type="text" class="input is-small">
-           <!-- We need to use variable "tracks" b/c we put it in our data() -->
-          <div class="tracks">
-
-          </div>
         </div>
-
+        <div class="panel-block" v-for="track in tracks" >
+           <!-- We need to use variable "tracks" b/c we put it in our data() -->
+          <song-item :track="track"></song-item>
+        </div>
       </div>
-
-
     </div>
   </div>
 </template>
@@ -28,7 +25,7 @@ export default {
     fetch('https://api.spotify.com/v1/search?query=katy&type=track&offset=0&limit=20')
     .then(response => response.json())
     .then((tracks) => {
-      this.tracks = tracks;
+      this.tracks = tracks.tracks.items;
     });
   },
 
--- app/components/song-item.vue
+++ app/components/song-item.vue
@@ -1,12 +1,12 @@
 <template lang="html">
     // we can call track anything
-  <div v-for="track in tracks" class="media">
+  <div class="media is-fullwidth">
     <div class="media-left">
-      <img src="#" alt="" class="media-img">
+      <img :src="track.album.images[0].url" alt="" class="media-img">
     </div>
     <div class="media-content">
-      <h2>{{tracks.items[0].album.artists[0].name}}</h2>
-      <p>{{tracks.items[0].album.name}}</p>
+      <h2>{{track.album.artists[0].name}}</h2>
+      <p>{{track.album.name}}</p>
     </div>
     <div class="media-right">
       <span class="icon is-small"><i class="fa fa-spotify" aria-hidden="true"></i></span>
@@ -20,6 +20,10 @@ export default {
   data() {
     return {
     };
+  },
+
+  created() {
+
   },
 
   methods: {
